911832139e46f7b17370e0290fa7058c2950056f
u116 <adrianmontero@tutanota.com>
2023-09-10 01:59:26 +0200

    Adding of errors at the login page

--- views/login/login.view.php
+++ views/login/login.view.php
@@ -1,10 +1,12 @@
 <form class="simple" action="/login" method="post">
     <p>Log into your account:</p>
-    <input type="text" name="u" id="u" placeholder="Username">
+    <?php if (isset($web['page']['var']['user']['username']['message'])) print "<p class='error'>".$web['page']['var']['user']['username']['message']."</p>"?>
+    <?php if (isset($web['page']['var']['user']['password']['message'])) print "<p class='error'>".$web['page']['var']['user']['password']['message']."</p>" ?>
+    <input type="text" name="u" id="u" placeholder="Username" value="<?php if (isset($web['page']['var']['user']['username']['username'])) print $web['page']['var']['user']['username']['username'] ?>">
     <input type="password" name="p" id="p" placeholder="Password">
     <input type="submit" name="s" id="s" value="Sign In">
 </form>
 
 <script>
-    document.getElementById("un").focus();
+    document.getElementById("u").focus();
 </script>
