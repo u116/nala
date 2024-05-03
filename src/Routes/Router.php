08f5ad62a9ed7dd39477baa429eaa725c277c811
nala <me@nala.dev>
2024-05-03 18:07:05 +0200

    Added possilibity of slug routing (edit/* routes are currently broken)

--- src/Routes/Router.php
+++ src/Routes/Router.php
@@ -8,32 +8,46 @@ class Router
 {
     public ?string $uri;
     public array $uriGuide;
+    public ?string $controller;
     private ?string $route;
     private static string $controllersPath = '\Src\Controllers\\';
 
     private static array $routes = [
-        '/' => 'home',
-        '/about' => 'about',
-        '/contact' => 'contact',
-        '/interests' => 'interests',
-        '/blog' => 'blog',
-        '/index' => 'index',
-        '/login' => 'login',
-        '/register' => 'register',
-        '/logout' => 'logout',
-        '/edit/about' => 'edit',
-        '/edit/contact' => 'edit',
-        '/edit/interests' => 'edit',
-        '/edit/blog' => 'edit',
-        '/edit/index' => 'edit',
+        'normal' => [
+            '/' => 'home',
+            'about' => 'about',
+            'contact' => 'contact',
+            'interests' => 'interests',
+            'blog' => 'blog',
+            'index' => 'index',
+            'login' => 'login',
+            'register' => 'register',
+            'logout' => 'logout',
+            'edit/about' => 'edit',
+            'edit/contact' => 'edit',
+            'edit/interests' => 'edit',
+            'edit/blog' => 'edit',
+            'edit/index' => 'edit',
+            'redirect' => 'redirect',
+        ],
+        'with-slug' => [
+            'r' => [
+                'route' => 'redirect',
+                'dynamic-slug' => true // To be managed by the controller.
+            ]
+        ]
     ];
     
 
     public function __construct()
     {
         $this->uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $this->uriGuide = explode('/', (string) $this->uri);
-        $this->route = self::$routes[$this->uri] ?? null;
+        $this->uriGuide = array_values(array_filter(explode('/', $this->uri)));
+        $this->route = $this->uriGuide[0] ?? '/';
+
+        $this->controller = self::$routes['normal'][$this->route]
+            ?? self::$routes['with-slug'][$this->route]['route']
+            ?? null;
     }
 
     private function controller($route): object
@@ -52,9 +66,15 @@ class Router
             'PUT'
         ])) return $this->abort(new ResponseController, 400);
 
-        if (is_null($this->route)) return $this->abort(new ResponseController);
+        if (is_null($this->controller)) return $this->abort(new ResponseController);
 
-        return $this->controller($this->route)->$method();
+        if (self::$routes['with-slug'][$this->route]['dynamic-slug'] ?? false) {
+            return $this
+                ->controller(self::$routes['with-slug'][$this->route]['route'])
+                ->get($this->uriGuide[1] ?? null);
+        }
+
+        return $this->controller($this->controller)->$method();
     }
 
     protected function abort(ResponseController $object, $httpCode = 404): array
